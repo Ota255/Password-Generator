c3802d6f8adf9af4b1bb8babff074294c246c458
Ota255 <63925771+Ota255@users.noreply.github.com>
2024-03-03 22:46:53 -0300

    Code Upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+passElL = document.getElementById("pass1")
+passElR = document.getElementById("pass2")
+generateBtn = document.getElementById("generate-btn") 
+settingsBtn = document.getElementById("settings-btn")
+
+PassLenght = document.getElementById("length")
+
+
+function passGen(){
+    let pLen = PassLenght.valueAsNumber
+    let result = ""
+    for (let i = 0; i < pLen ; i++){
+        let rNum = Math.floor(Math.random() * characters.length)
+        result += characters[rNum]
+    }
+    return result
+}
+generateBtn.addEventListener("click", setElements)
+
+function setElements(){
+    passElL.textContent = passGen()
+    passElR.textContent = passGen()
+}
+
+function copyText(textC){
+    navigator.clipboard.writeText(textC);
+}
+
+passElL.addEventListener("click", () => copyText(passElL.textContent))
+
+settingsBtn.addEventListener("click", toggleSettings)
+function toggleSettings() {
+    let settingsEl = document.getElementById("settings-div")
+    if(settingsEl.style.display === 'none'){
+        settingsEl.style.display = 'flex'
+    } else {
+        settingsEl.style.display = 'none'
+    }
+}
