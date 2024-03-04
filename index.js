12dfae229f808b04ed5ce73d2e79003188915068
Ota255 <63925771+Ota255@users.noreply.github.com>
2024-03-03 23:15:08 -0300

    Update

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const charNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const charSpecial = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
-passElL = document.getElementById("pass1")
-passElR = document.getElementById("pass2")
-generateBtn = document.getElementById("generate-btn") 
-settingsBtn = document.getElementById("settings-btn")
 
-PassLenght = document.getElementById("length")
+let passElL = document.getElementById("pass1")
+let passElR = document.getElementById("pass2")
+let generateBtn = document.getElementById("generate-btn") 
+let settingsBtn = document.getElementById("settings-btn")
 
+let PassLenght = document.getElementById("length")
+let hasNumbers = document.getElementById("hasNumber")
+let hasSpecial = document.getElementById("hasSymbols")
+console.log(hasNumbers)
 
 function passGen(){
     let pLen = PassLenght.valueAsNumber
+    let char = characters
+    if (hasNumbers.checked === true){
+        char = char.concat(charNums)
+    }
+    if (hasSpecial.checked === true){
+        char = char.concat(charSpecial)
+    }
+    console.log(char)
     let result = ""
     for (let i = 0; i < pLen ; i++){
-        let rNum = Math.floor(Math.random() * characters.length)
-        result += characters[rNum]
+        let rNum = Math.floor(Math.random() * char.length)
+        result += char[rNum]
     }
     return result
 }
@@ -30,6 +42,7 @@ function copyText(textC){
 }
 
 passElL.addEventListener("click", () => copyText(passElL.textContent))
+passElR.addEventListener("click", () => copyText(passElR.textContent))
 
 settingsBtn.addEventListener("click", toggleSettings)
 function toggleSettings() {
